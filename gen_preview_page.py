1615b9e717a7b9a862176cd8aa63f5d671c81f6e
Jackon Yang <i@jackon.me>
2020-02-10 22:58:35 +0800

    add preview-page

---
+++ gen_preview_page.py
@@ -0,0 +1,37 @@
+import os
+
+def get_cate(data_root):
+    for i in sorted(os.listdir(data_root)):
+        if i.startswith('.'):
+            continue
+        yield i, os.path.join(data_root, i)
+
+
+def get_list(data_root):
+    for cate, cate_path in get_cate(data_root):
+        yield '## %s' % cate
+
+        for filename in sorted(os.listdir(cate_path)):
+            if filename.startswith('.'):
+                continue
+            if filename.lower().endswith('.jpg') or filename.lower().endswith('.png'):
+                yield (
+                    '#### %s\n\n'
+                    '<img src="%s" width="256">'
+                ) % (
+                    filename,
+                    os.path.join(cate_path, filename)
+                )
+            else:
+                yield '#### %s: <a href="%s"> 播放视频 </a>' % (
+                    filename,
+                    os.path.join(cate_path, filename)
+                )
+
+def main(data_root):
+    with open('data_preview.md', 'w') as f:
+        f.write('\n\n'.join([text for text in get_list(data_root)]))
+
+
+if __name__ == '__main__':
+    main('图片视频素材')
